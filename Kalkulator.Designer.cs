0ee0346e97d02a208af268838f893bfbe673203a
Gubinator <59795219+Gubinator@users.noreply.github.com>
2020-01-12 16:25:00 +0100

    Add files via upload

---
+++ Kalkulator.Designer.cs
@@ -0,0 +1,372 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Kalkulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bDijeljenje = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.bMnozenje = new System.Windows.Forms.Button();
+            this.bZbrajanje = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.bClearE = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bOduzimanje = new System.Windows.Forms.Button();
+            this.rezultat = new System.Windows.Forms.TextBox();
+            this.bClear = new System.Windows.Forms.Button();
+            this.bJednako = new System.Windows.Forms.Button();
+            this.btnSin = new System.Windows.Forms.Button();
+            this.btnCos = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnExp = new System.Windows.Forms.Button();
+            this.btnSqrt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(33, 89);
+            this.b7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(49, 46);
+            this.b7.TabIndex = 0;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(93, 89);
+            this.b8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(49, 46);
+            this.b8.TabIndex = 1;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(155, 89);
+            this.b9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(49, 46);
+            this.b9.TabIndex = 2;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bDijeljenje
+            // 
+            this.bDijeljenje.Location = new System.Drawing.Point(223, 89);
+            this.bDijeljenje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bDijeljenje.Name = "bDijeljenje";
+            this.bDijeljenje.Size = new System.Drawing.Size(68, 46);
+            this.bDijeljenje.TabIndex = 3;
+            this.bDijeljenje.Text = "/";
+            this.bDijeljenje.UseVisualStyleBackColor = true;
+            this.bDijeljenje.Click += new System.EventHandler(this.operator_click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(33, 157);
+            this.b4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(49, 46);
+            this.b4.TabIndex = 4;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(93, 157);
+            this.b5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(49, 46);
+            this.b5.TabIndex = 5;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(155, 157);
+            this.b6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(49, 46);
+            this.b6.TabIndex = 6;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bMnozenje
+            // 
+            this.bMnozenje.Location = new System.Drawing.Point(223, 157);
+            this.bMnozenje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bMnozenje.Name = "bMnozenje";
+            this.bMnozenje.Size = new System.Drawing.Size(68, 46);
+            this.bMnozenje.TabIndex = 7;
+            this.bMnozenje.Text = "*";
+            this.bMnozenje.UseVisualStyleBackColor = true;
+            this.bMnozenje.Click += new System.EventHandler(this.operator_click);
+            // 
+            // bZbrajanje
+            // 
+            this.bZbrajanje.Location = new System.Drawing.Point(223, 223);
+            this.bZbrajanje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bZbrajanje.Name = "bZbrajanje";
+            this.bZbrajanje.Size = new System.Drawing.Size(68, 46);
+            this.bZbrajanje.TabIndex = 8;
+            this.bZbrajanje.Text = "+";
+            this.bZbrajanje.UseVisualStyleBackColor = true;
+            this.bZbrajanje.Click += new System.EventHandler(this.operator_click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(155, 223);
+            this.b3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(49, 46);
+            this.b3.TabIndex = 9;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(93, 223);
+            this.b2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(49, 46);
+            this.b2.TabIndex = 10;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(33, 223);
+            this.b1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(49, 46);
+            this.b1.TabIndex = 11;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bClearE
+            // 
+            this.bClearE.Location = new System.Drawing.Point(312, 89);
+            this.bClearE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bClearE.Name = "bClearE";
+            this.bClearE.Size = new System.Drawing.Size(66, 46);
+            this.bClearE.TabIndex = 12;
+            this.bClearE.Text = "CE";
+            this.bClearE.UseVisualStyleBackColor = true;
+            this.bClearE.Click += new System.EventHandler(this.bClearE_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(33, 288);
+            this.b0.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(109, 46);
+            this.b0.TabIndex = 13;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.button_Click);
+            // 
+            // bOduzimanje
+            // 
+            this.bOduzimanje.Location = new System.Drawing.Point(223, 288);
+            this.bOduzimanje.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bOduzimanje.Name = "bOduzimanje";
+            this.bOduzimanje.Size = new System.Drawing.Size(68, 46);
+            this.bOduzimanje.TabIndex = 15;
+            this.bOduzimanje.Text = "-";
+            this.bOduzimanje.UseVisualStyleBackColor = true;
+            this.bOduzimanje.Click += new System.EventHandler(this.operator_click);
+            // 
+            // rezultat
+            // 
+            this.rezultat.Location = new System.Drawing.Point(33, 46);
+            this.rezultat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rezultat.Name = "rezultat";
+            this.rezultat.Size = new System.Drawing.Size(259, 20);
+            this.rezultat.TabIndex = 16;
+            this.rezultat.Text = "0";
+            this.rezultat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.rezultat.TextChanged += new System.EventHandler(this.rezultat_TextChanged);
+            // 
+            // bClear
+            // 
+            this.bClear.Location = new System.Drawing.Point(312, 157);
+            this.bClear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bClear.Name = "bClear";
+            this.bClear.Size = new System.Drawing.Size(66, 46);
+            this.bClear.TabIndex = 17;
+            this.bClear.Text = "C";
+            this.bClear.UseVisualStyleBackColor = true;
+            this.bClear.Click += new System.EventHandler(this.bClear_Click);
+            // 
+            // bJednako
+            // 
+            this.bJednako.Location = new System.Drawing.Point(312, 223);
+            this.bJednako.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bJednako.Name = "bJednako";
+            this.bJednako.Size = new System.Drawing.Size(66, 110);
+            this.bJednako.TabIndex = 18;
+            this.bJednako.Text = "=";
+            this.bJednako.UseVisualStyleBackColor = true;
+            this.bJednako.Click += new System.EventHandler(this.bJednako_Click);
+            // 
+            // btnSin
+            // 
+            this.btnSin.Location = new System.Drawing.Point(394, 89);
+            this.btnSin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSin.Name = "btnSin";
+            this.btnSin.Size = new System.Drawing.Size(66, 46);
+            this.btnSin.TabIndex = 19;
+            this.btnSin.Text = "sin";
+            this.btnSin.UseVisualStyleBackColor = true;
+            this.btnSin.Click += new System.EventHandler(this.btnSin_Click);
+            // 
+            // btnCos
+            // 
+            this.btnCos.Location = new System.Drawing.Point(394, 157);
+            this.btnCos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnCos.Name = "btnCos";
+            this.btnCos.Size = new System.Drawing.Size(66, 46);
+            this.btnCos.TabIndex = 20;
+            this.btnCos.Text = "cos";
+            this.btnCos.UseVisualStyleBackColor = true;
+            this.btnCos.Click += new System.EventHandler(this.btnCos_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(478, 89);
+            this.btnLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(66, 46);
+            this.btnLog.TabIndex = 21;
+            this.btnLog.Text = "log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnExp
+            // 
+            this.btnExp.Location = new System.Drawing.Point(478, 157);
+            this.btnExp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnExp.Name = "btnExp";
+            this.btnExp.Size = new System.Drawing.Size(66, 46);
+            this.btnExp.TabIndex = 22;
+            this.btnExp.Text = "Exp";
+            this.btnExp.UseVisualStyleBackColor = true;
+            this.btnExp.Click += new System.EventHandler(this.btnExp_Click);
+            // 
+            // btnSqrt
+            // 
+            this.btnSqrt.Location = new System.Drawing.Point(394, 223);
+            this.btnSqrt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSqrt.Name = "btnSqrt";
+            this.btnSqrt.Size = new System.Drawing.Size(66, 46);
+            this.btnSqrt.TabIndex = 23;
+            this.btnSqrt.Text = "sqrt";
+            this.btnSqrt.UseVisualStyleBackColor = true;
+            this.btnSqrt.Click += new System.EventHandler(this.btnSqrt_Click);
+            // 
+            // Kalkulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 458);
+            this.Controls.Add(this.btnSqrt);
+            this.Controls.Add(this.btnExp);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnCos);
+            this.Controls.Add(this.btnSin);
+            this.Controls.Add(this.bJednako);
+            this.Controls.Add(this.bClear);
+            this.Controls.Add(this.rezultat);
+            this.Controls.Add(this.bOduzimanje);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bClearE);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.bZbrajanje);
+            this.Controls.Add(this.bMnozenje);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.bDijeljenje);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Kalkulator";
+            this.Text = "Kalkulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bDijeljenje;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button bMnozenje;
+        private System.Windows.Forms.Button bZbrajanje;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button bClearE;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bOduzimanje;
+        private System.Windows.Forms.TextBox rezultat;
+        private System.Windows.Forms.Button bClear;
+        private System.Windows.Forms.Button bJednako;
+        private System.Windows.Forms.Button btnSin;
+        private System.Windows.Forms.Button btnCos;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnExp;
+        private System.Windows.Forms.Button btnSqrt;
+    }
+}
+
---
+++ Kalkulator.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Kalkulator : Form
+    {
+        double vrijednost = 0;
+        string operacija = "";
+        bool operacija_stisnita = false;
+        public Kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ( (rezultat.Text == "0") || (operacija_stisnita) )
+            {
+                rezultat.Clear();
+            }
+
+            Button b = (Button)sender;
+            if (b.Text == ",")
+            {
+                if(!rezultat.Text.Contains(","))
+                    rezultat.Text = rezultat.Text + b.Text;
+
+            }
+            else
+            {
+                rezultat.Text = rezultat.Text + b.Text;
+            }
+           
+        }
+
+        private void bClearE_Click(object sender, EventArgs e)
+        {
+            rezultat.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operacija = b.Text;
+            vrijednost = double.Parse(rezultat.Text);
+            operacija_stisnita = true;
+        }
+
+        private void bJednako_Click(object sender, EventArgs e)
+        {
+            switch (operacija)
+            {
+                case "+":
+                    rezultat.Text = (vrijednost + double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "-":
+                    rezultat.Text = (vrijednost - double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "/":
+                    rezultat.Text = (vrijednost / double.Parse(rezultat.Text)).ToString();
+                    break;
+                case "*":
+                    rezultat.Text = (vrijednost * double.Parse(rezultat.Text)).ToString();
+                    break;
+       
+                default:
+                    break;
+
+            }
+            operacija_stisnita = false;
+        }
+
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            rezultat.Clear();
+            vrijednost = 0;
+        }
+
+        private void btnCos_Click(object sender, EventArgs e)
+        {
+            double num = double.Parse(rezultat.Text);
+            num = Math.Cos(num);
+            rezultat.Text = System.Convert.ToString(num);
+
+        }
+
+        private void btnSin_Click(object sender, EventArgs e)
+        {
+            double num = double.Parse(rezultat.Text);
+            num = Math.Sin(num);
+            rezultat.Text = System.Convert.ToString(num);
+        }
+
+        private void btnExp_Click(object sender, EventArgs e)
+        {
+            double broj = double.Parse(rezultat.Text);
+            rezultat.Text = (Math.Exp(broj)).ToString();
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            double ilog = double.Parse(rezultat.Text);
+            ilog = Math.Log10(ilog);
+            rezultat.Text = System.Convert.ToString(ilog);
+        }
+
+        private void btnSqrt_Click(object sender, EventArgs e)
+        {
+            double num= double.Parse(rezultat.Text);
+            num = Math.Sqrt(num);
+            rezultat.Text = System.Convert.ToString(num);
+        }
+
+        private void rezultat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
